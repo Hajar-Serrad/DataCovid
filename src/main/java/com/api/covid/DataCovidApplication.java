324533e989c1e62ab7f020c56158814174312929
Hajar-Serrad <hajarserrad@gmail.com>
2022-01-31 16:24:22 +0100

    first commit

---
+++ src/main/java/com/api/covid/DataCovidApplication.java
@@ -0,0 +1,13 @@
+package com.api.covid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCovidApplication.class, args);
+	}
+
+}
